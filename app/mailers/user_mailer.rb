bdce1c7ab35a7a9350c5a8bf32f189178d958d29
Julien Janson <juljanson@gmail.com>
2014-01-06 21:41:23 +0100

    sending email

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ActionMailer::Base
+  
+  default from: "do-not-replyexample.com"
+
+  def contact_email(contact)
+  	@contact = contact
+  	mail(to: ENV["OWNER_EMAIL"], from: @contact.email, :subject => "Website Contact")
+  end
+
+end
