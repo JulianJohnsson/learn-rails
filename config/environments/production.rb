82e069e336279bde21feb5d3b54dcb7fb5056ab1
Julien Janson <juljanson@gmail.com>
2014-01-09 22:13:13 +0100

    configuration production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,9 @@ LearnRails::Application.configure do
   config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"] } 
   config.action_mailer.delivery_method = :smtp 
   config.action_mailer.perform_deliveries = true 
-  config.action_mailer.raise_delivery_errors = false config.action_mailer.default :charset => "utf-8" config.action_mailer.smtp_settings = {
+  config.action_mailer.raise_delivery_errors = false 
+  config.action_mailer.default :charset => "utf-8" 
+  config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: ENV["DOMAIN_NAME"], 
